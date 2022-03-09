bccca6cb422576b92bb5cde70ff857a59784434b
Truong Minh Hong <truongminhhong0306@gmail.com>
2022-03-09 10:54:04 +0700

    add main, settings

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -28,6 +28,7 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -75,11 +76,14 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'my_project',
+        'USER': 'hongtr',
+        'PASSWORD': 'Passforhongtrmysql@@0101',
+        'HOST': 'localhost',   
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
