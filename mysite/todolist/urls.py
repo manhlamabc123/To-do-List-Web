415bc347354295a873bac49422b64600567542ee
manhlamabc123 <tranducmanh12092001@gmail.com>
2022-03-15 21:18:24 +0700

    Change form Text to Input in HTML using HTMX

--- mysite/todolist/urls.py
+++ mysite/todolist/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('create_new_todolist_<str:list_name>/', views.create_new_todolist),
     path('delete_todolist_<str:list_name>/', views.delete_todolist),
     path('deleted_todolist/', views.deleted_todolist),
+    path('clicked_todolist_name/', views.clicked_todolist_name),
+    path('edit_todolist_name/', views.clicked_todolist_name),
 ]
--- mysite/todolist/views.py
+++ mysite/todolist/views.py
@@ -1,4 +1,5 @@
 from django.contrib import messages
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import redirect, render
 from todolist.models import ToDoLists
 
@@ -32,3 +33,25 @@ def deleted_todolist(response):
         "user_todolist": user_todolist,
     }
     return render(response, "todolist/deleted_todolist.html", context)
+
+def clicked_todolist_name(response):
+    replace_with = '''<div 
+                        class="card-header-title font-size-lg text-capitalize font-weight-normal"
+                    ><i class=\"fa fa-tasks\"></i>&nbsp;
+                        <form action=\"/edit_todolist_name\">\n
+                            <div class=\"input-group input-group-sm\">\n
+                                <input type=\"text\" class=\"form-control\" placeholder=\"New name\" name="new_name">\n
+                                <button 
+                                    class=\"btn btn-primary btn-sm\" 
+                                    type=\"button\"
+                                >Save</button>\n
+                            </div>\n
+                        </form>
+                    </div>'''
+    return HttpResponse(replace_with)
+
+def edit_todolist_name(response):
+    if response.method == "POST":
+        new_name = response.POST.get("new_name")
+        print(new_name)
+    return HttpResponse(new_name)
