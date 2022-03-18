af3ed257483ace287b30be32dc36e74cd1823b6d
manhlamabc123 <tranducmanh12092001@gmail.com>
2022-03-18 10:28:59 +0700

    Create Habit app

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -20,5 +20,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("main.urls")),
     path('', include("todolist.urls")),
+    path('', include("habit.urls")),
     path('', include("django.contrib.auth.urls")),
 ]
