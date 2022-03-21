009d9f5dee164941cc8976d9ae76716eece79ab8
manhlamabc123 <tranducmanh12092001@gmail.com>
2022-03-21 18:15:53 +0700

    Delete Habit, Fix bug: No user bug

--- mysite/habit/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def habit(response):
-    return render(response, "habit/habit.html", {})
--- mysite/main/views.py
+++ mysite/main/views.py
@@ -3,5 +3,8 @@ from todolist.models import ToDoLists
 
 # Create your views here.
 def home(response):
-    user_first_todolist = ToDoLists.UserFirstList(response.user.id)
+    if response.user.is_authenticated:
+        user_first_todolist = ToDoLists.UserFirstList(response.user.id)
+    else:
+        user_first_todolist = None
     return render(response, "main/home.html", {"user_first_todolist": user_first_todolist})
